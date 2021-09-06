431169621aaae4560372d01116e702c8e951138e
Emeraude <animekamisama@gmail.com>
2021-09-06 19:30:02 -0400

    updated

---
+++ cs415_Assignment1.py
@@ -0,0 +1,14 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+df = pd.DataFrame({"C1": [14.23, 13.20, 13.16, 14.37, 13.24],
+                   "C2": [1.71, 1.78, 2.36, 1.95, 2.59],
+                   "C3": [2.43, 2.14, 2.67, 2.50, 2.87],
+                   "C4": [15.6, 11.2, 18.6, 16.8, 21.0],
+                   "C5": [127, 100, 101, 113, 118]})
+
+print(df)
+print(df.cov())
+print(df.var())
